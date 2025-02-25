995926af06c1c630b5785e78f761e7429d4a3864
haaidofficial <haaidrehman372@gmail.com>
2025-02-25 22:11:05 +0530

    next config updated

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -3,7 +3,7 @@ const SERVER_HOSTNAME = process.env.NEXT_PUBLIC_SERVER_HOSTNAME;
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['localhost', SERVER_HOSTNAME, 'www.sterlingfares.com'],
+    domains: ['localhost',],
   },
 };
 
